cc5b61b702c4795892d5a0db4e4a65c40eebc9ba
Arik Ruiz <afruiz@syr.edu>
2023-10-03 09:19:36 -0400

    Updates

--- Task2.py
+++ Task2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-wb = pd.read_excel('Project1.xlsx', dtype=str)
+wb = pd.read_excel('F23P1-M010-Group9.xlsx', dtype=str)
 bins = list(wb["Bin"])
 chars = list(wb["Char"])
 chars[13] = "\n"
@@ -44,12 +44,15 @@ def returnBinary(p1: str) -> tuple:
                 return binVal, p1
     # Checks for one character
     elif len(p1) >= 1:
-        s1 = p1[0:1]
         for i in range(len(chars)):
-            if chars[i] == s1:
-                p1 = p1[1:]
+            if chars[i] == p1[0]:
+                if len(p1) > 1:
+                    p1 = p1[1:]
+                else:
+                    p1 = ""
                 binVal = bins[i]
                 return binVal, p1
+            
     # returns an empty string if given an empty string
     elif len(p1) == 0:
         return ""
--- Task6.py
+++ Task6.py
@@ -1,34 +1,35 @@
 import pandas as pd
 
-wb = pd.read_excel('Project1.xlsx', dtype=str)
+wb = pd.read_excel('F23P1-M010-Group9.xlsx', dtype=str)
 bins = list(wb["Bin"])
 chars = list(wb["Char"])
 chars[13] = "\n"
 
 
-def check_if_identical(p1: str, p2: str) -> bool:
+def check_if_identical() -> bool: # or keep p1: str in ()
     # defaults p2 to TextOutput.txt
-    p2 = "TextOutput.txt"
+    p1 = str(input("What's the file name for p1?"))
+    p2 = "TextOutput"
 
-    # for p1, opens and reads p1 into a string and prints it
+    # for p1, opens and reads p1 into a string
     f = open(p1)
     s1 = f.read()
-    print(s1)
 
-    # for p2, opens and reads p1 into a string and prints it
+    # for p2, opens and reads p1 into a string
     g = open(p2)
     s2 = g.read()
-    print(s2)
 
     # checks if the two strings/files are different
     same = True
     i = 0
-    while same:
-        if s1[i] == s2[i]:
-            print("Still same")
+    while True:
+        if len(s1) < i or len(s2) < i:
+            break
+        elif s1[i] == s2[i]:
+            i += 1
+            continue
         else:
             same = False
-        i += 1
 
     # closes both files - at the end
     f.close()
